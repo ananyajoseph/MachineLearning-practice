44efc3f10ae59b2c00f2c09a2dfe9a33807892c2
ananyajoseph <ananyajosephc@gmail.com>
2025-02-12 15:31:08 -0600

    Update Data Imbalance Techniques.py

--- Data Imbalance Techniques.py
+++ Data Imbalance Techniques.py
@@ -1,5 +1,4 @@
-#%%
-!pip install imblearn
+
 #%%
 # Importing necessary libraries
 import pandas as pd
@@ -11,10 +10,72 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from imblearn.over_sampling import SMOTE
 # %%
-df_credit = pd.read_csv('/Users/anany/OneDrive/Desktop/SEM2/6302-machine learning/winequality-red.csv')
+df_credit = pd.read_csv('/Users/anany/OneDrive/Desktop/SEM2/6302-machine learning/creditcard.csv')
+
+#%%
+df_credit.head()
 #%%
 class_0 = df_credit[df_credit['Class'] == 0]
 class_1 = df_credit[df_credit['Class'] == 1]
 print('class 0:', class_0.shape)
 print('class_1:', class_1.shape )
 # how to calculate the % from this? [class_1.shape[0]/df_credit.shape[0]]
+
+# %%
+# Seperate my target and variables(independent)
+x = df_credit.drop('Class', axis = 1)
+y = df_credit['Class']
+
+# %%
+# Apply undersampling to the majority class
+under_sampler = RandomUnderSampler(random_state=10,
+replacement=True)
+
+x_under, y_under = under_sampler.fit_resample(x, y)
+
+# %%
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+test_size = 0.2,
+random_state = 10)
+
+#%%
+x_train, x_test, y_train, y_test = train_test_split(x_under, y_under,
+test_size = 0.2,
+random_state = 10)
+
+# %%
+clf = RandomForestClassifier(random_state = 10)
+clf.fit(x_train, y_train)
+# %%
+y_pred = clf.predict(x_test)
+
+#%%
+#Evaluation of the model
+print(confusion_matrix(y_test, y_pred))
+
+#%%
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+# %%
+print(classification_report(y_test,y_pred))
+
+# %%
+# SMOTE
+
+smote = SMOTE(random_state = 10)
+x_smote, y_smote = smote.fit_resample(x, y)
+# %%
+# %%
+x_train, x_test, y_train, y_test = train_test_split(x_smote, y_smote,
+test_size = 0.2,
+random_state = 10)
+# %%
+clf = RandomForestClassifier(random_state = 10)
+clf.fit(x_train, y_train)
+# %%
+y_pred = clf.predict(x_test)
+
+#%%
+#Evaluation of the model
+print(confusion_matrix(y_test, y_pred))
+# %%
+print(classification_report(y_test,y_pred))
